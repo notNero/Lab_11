dbd75faa2204e40c8c4789447ee3994633cb6ad4
notNero <kronat83@gmail.com>
2023-05-28 16:11:43 +0300

    Change file JoinButton.kt

--- app/src/main/java/com/topic3/android/reddit/components/JoinButton.kt
+++ app/src/main/java/com/topic3/android/reddit/components/JoinButton.kt
@@ -1,5 +1,7 @@
 package com.topic3.android.reddit.components
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -31,10 +33,17 @@ fun JoinButton(onClick: (Boolean) -> Unit = {}){
 
     val shape = RoundedCornerShape(corner = CornerSize(12.dp))
 
-    val buttonBackgroundColor: Color =
+    val transition = updateTransition(
+        targetState = buttonState,
+        label = "JoinButtonTransition"
+    )
+
+    val buttonBackgroundColor: Color by animateColorAsState(
         if (buttonState == JoinButtonState.PRESSED)
-            Color.White else
+            Color.White
+        else
             Color.Blue
+    )
 
     val iconAsset: ImageVector =
         if (buttonState == JoinButtonState.PRESSED)
